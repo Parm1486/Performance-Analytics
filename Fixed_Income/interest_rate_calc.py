351a4826386a17a8e2545c92b3a2e65a3ea13a87
Parminder Singh <159745692+Parm1486@users.noreply.github.com>
2026-01-13 17:31:45 -0800

    Create interest_rate_calc.py

---
+++ Fixed_Income/interest_rate_calc.py
@@ -0,0 +1,84 @@
+# Calculate Annual Interest Rate from Instantaneous Interest Rate.
+
+def inst_to_ann(r):
+    """
+    Convert and instantaneous interest rate to an annual interest rate
+    """
+    return np.expm1(r)
+
+## Calculate Instantaneous Interest Rate from Annual Interest Rate.
+
+def ann_to_inst(r):
+    """
+    Convert and instantaneous interest rate to an annual interest rate
+    """
+    return np.log1p(r)
+
+### Calculate Bond Discount Rate.
+
+def discount(t, r):
+    """
+    Compute the price of a pure discount bond that pays a dollar at time period t
+    and r is the per-period interest rate
+    returns a |t| x |r| Series or DataFrame
+    r can be a float, Series or DataFrame
+    returns a DataFrame indexed by t
+    """
+    discounts = pd.DataFrame([(r+1)**-i for i in t])
+    discounts.index = t
+    return discounts
+
+#### Calcualte Present value of Future Cash Flows.
+
+def pv(flows, r):
+    """
+    Compute the present value of a sequence of cash flows given by the time(as an index) and amounts
+    r can be scalar, or a Series or DataFrame with the number of rows matching the num of rows in flows
+    """
+    dates = flows.index
+    discounts = discount(dates, r)
+    return discounts.multiply(flows, axis='rows').sum()
+
+##### Calculate Random Interest Rate Using CIR Model
+
+import math
+def cir(n_years = 10, n_scenarios =1, a=0.05, b=0.03, sigma=0.05, steps_per_year=12, r_0=None):
+    """
+    Generate random interest rate evolution over time using the CIR model
+    b and r_0 are assumed to be the annualized rates, not the short rate
+    and the returned values are the annualized rates as well
+    """
+    if r_0 is None: r_0 = b
+    r_0 = ann_to_inst(r_0)
+    dt = 1/steps_per_year
+    num_steps = int(n_years*steps_per_year) + 1 #because n_years might be a float
+
+    shock = np.random.normal(0, scale=np.sqrt(dt), size=(num_steps, n_scenarios))
+    rates = np.empty_like(shock)
+    rates[0] = r_0
+
+    ## For Price Generation
+    h= math.sqrt(a**2 + 2*sigma**2)
+    prices = np.empty_like(shock)
+    ###
+
+    def price(ttm, r):
+        _A = ((2*h*math.exp((h+a)*ttm/2))/(2*h+(h+a)*(math.exp(h*ttm)-1)))**(2*a*b/sigma**2)
+        _B = (2*(math.exp(h*ttm)-1))/(2*h + (h+a)*(math.exp(h*ttm)-1))
+        _P = _A*np.exp(-_B*r)
+        return _P
+    prices[0] = price(n_years, r_0)
+    ###
+
+    for step in range(1, num_steps):
+        r_t = rates[step-1]
+        d_r_t = a*(b-r_t)*dt + sigma*np.sqrt(r_t)*shock[step]
+        rates[step] = abs(r_t + d_r_t)
+        # generate prices at time t as well...
+        prices[step] = price(n_years-step*dt, rates[step])
+
+    rates = pd.DataFrame(data=inst_to_ann(rates), index=range(num_steps))
+    ### for prices
+    prices = pd.DataFrame(data=prices, index=range(num_steps))
+    ###
+    return rates, prices
