4516cefaf00c91b30eb8647203546d40db3a1a0a
Parminder Singh <159745692+Parm1486@users.noreply.github.com>
2026-01-13 17:11:38 -0800

    Create portfolio_risk_adjusted_return_calc.py

---
+++ Public_Markets/portfolio_risk_adjusted_return_calc.py
@@ -0,0 +1,12 @@
+# Calculate Sharpe Ratio of Portfolio
+
+def sharpe_ratio(r, riskfree_rate, periods_per_year):
+    """
+    Computes the annualized sharpe ratio of a set of returns
+    """
+    # convert the annual riskfree rate to per period
+    rf_per_period = (1+riskfree_rate)**(1/periods_per_year)-1
+    excess_ret = r - rf_per_period
+    ann_ex_ret = annualize_rets(excess_ret, periods_per_year)
+    ann_vol = annualize_vol(r, periods_per_year)
+    return ann_ex_ret/ann_vol
